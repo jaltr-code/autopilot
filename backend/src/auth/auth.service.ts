68956d3063569893bbb24e0ce63ef9d11ff8847d
jaltr-code <int.europe.ahmed@gmail.com>
2026-03-07 23:44:24 +0000

    Implemented the sign up flow with the user roles

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { RoleName } from '../../generated/prisma/enums';
+import { PrismaService } from '../prisma/prisma.service';
+import { SignupDto } from './dto/signup.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(data: SignupDto) {
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('Email already registered');
+    }
+
+    const existingCompany = await this.prisma.company.findUnique({
+      where: {
+        slug: data.companySlug,
+      },
+    });
+
+    if (existingCompany) {
+      throw new BadRequestException('Company slug already in use');
+    }
+
+    const passwordHash = await bcrypt.hash(data.password, 10);
+
+    const result = await this.prisma.$transaction(async (tx) => {
+      const company = await tx.company.create({
+        data: {
+          name: data.companyName,
+          slug: data.companySlug,
+        },
+      });
+
+      const staffRole = await tx.role.create({
+        data: {
+          companyId: company.id,
+          name: RoleName.STAFF,
+        },
+      });
+
+      await tx.role.create({
+        data: {
+          companyId: company.id,
+          name: RoleName.TEAM_LEAD,
+        },
+      });
+
+      await tx.role.create({
+        data: {
+          companyId: company.id,
+          name: RoleName.MANAGER,
+        },
+      });
+
+      const adminRole = await tx.role.create({
+        data: {
+          companyId: company.id,
+          name: RoleName.ADMIN,
+        },
+      });
+
+      const user = await tx.user.create({
+        data: {
+          email: data.email,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          passwordHash,
+          companyId: company.id,
+          roleId: adminRole.id,
+        },
+      });
+
+      return {
+        company,
+        user,
+      };
+    });
+
+    return {
+      message: 'Signup successful',
+      companyId: result.company.id,
+      userId: result.user.id,
+    };
+  }
+}
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -5,6 +5,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
+
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL!,
     });
